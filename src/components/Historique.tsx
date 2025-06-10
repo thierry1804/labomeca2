3076ca5b05ec89c282f1a3c2d61a98ef90a23f5c
Thierry <thierry1804@gmail.com>
2025-06-10 21:05:02 +0300

    about 1

--- src/components/Historique.tsx
+++ src/components/Historique.tsx
@@ -4,10 +4,10 @@ const Historique: React.FC = () => {
   return (
     <div className="min-h-screen bg-black text-white py-16">
       <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold mb-8 text-center">Historique</h1>
+        <h1 className="text-4xl font-bold mb-8 text-center">Notre <span className="text-[#F67122]">histoire</span></h1>
         <div className="max-w-3xl mx-auto space-y-6">
           <p className="text-lg leading-relaxed">
-            Patrick Labouesse, fondateur de Labomeca a créé en 2002 à Antananarivo, son atelier d'usinage, c'est-à-dire de tournage, de fraisage et de rectification, en apportant ses connaissances et son savoir-faire acquis pendant 10 années à Heidelberg Allemagne dans le laboratoire EMBL de biologie moléculaire où il assurait la partie micromécanique.
+            <strong className="text-[#F67122]">Patrick Labouesse</strong>, fondateur de Labomeca a créé en 2002 à Antananarivo, son atelier d'usinage, c'est-à-dire de tournage, de fraisage et de rectification, en apportant ses connaissances et son savoir-faire acquis pendant 10 années à Heidelberg Allemagne dans le laboratoire EMBL de biologie moléculaire où il assurait la partie micromécanique.
           </p>
           <p className="text-lg leading-relaxed">
             Cette expérience professionnelle à la fois de l'acquisition de la connaissance des matières premières, du traitement thermique et leur application lui a permis de mettre en œuvre une gamme multifonction d'une grande qualité au service de l'industrie malgache.
